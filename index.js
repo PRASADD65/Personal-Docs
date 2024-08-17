dc25ce5720703152c44bebfd74dac7034a40a009
abhishekklabhi6 <109427930+abhishekklabhi6@users.noreply.github.com>
2024-08-17 16:09:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("sec").addEventListener("click",function()
+{
+    document.getElementById("sec").setAttribute("href","./index.html");
+}
+);
+
+function grtalert()
+{
+    img
+}
